bb46101c04bb603c20c1e9db24975f3b2ff5fe10
Matthew-Goley <nutmeg278@pm.me>
2026-04-02 23:38:48 -0500

    Fixed styling issues in Lens

--- vector/pages/lens_page.py
+++ vector/pages/lens_page.py
@@ -502,7 +502,7 @@ class _CTAReportCard(QFrame):
         self._scroll.setFrameShape(QFrame.Shape.NoFrame)
         self._scroll.setStyleSheet('QScrollArea { background: transparent; border: none; }')
         self._scroll.setMinimumHeight(120)
-        self._scroll.setMaximumHeight(500)
+        self._scroll.setMaximumHeight(700)
 
         items_container = QWidget()
         items_container.setStyleSheet('background: transparent;')
